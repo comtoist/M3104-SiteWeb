c8c4e90049b266def73e85edfe8f7e42695714c5
BULOT Andy <bulota@hostname.iut2.upmf-grenoble.fr>
2018-10-25 09:15:48 +0200

    new file:   PageAccueil.view.php 	new file:   design/PageAccueil.css

---
+++ view/PageAccueil.view.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Page-Accueil</title>
+  <link rel="stylesheet" type="text/css" href="design/PageAccueil.css">
+</head>
+<body>
+  <header>
+    <h1>NomSite</h1>
+    <nav>
+      <input type="text" placeholder="Search..">
+    </nav>
+  </header>
+
+  <section class="conteneur">
+    <section class="sousconteneur">
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+    </section>
+
+    <section class="sousconteneur">
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+    </section>
+
+    <section class="sousconteneur">
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+      <div>
+        <img src="" alt="image1">
+        <p>Categorie 1</p>
+      </div>
+    </section>
+
+  </section>
+  <footer></footer>
+</body>
+</html>
