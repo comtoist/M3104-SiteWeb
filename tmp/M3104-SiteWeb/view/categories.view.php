6009c29e01520c4b3a2e25a007d2e7715715b65b
FERNANDEZ Thibaud <fernathi@hostname.iut2.upmf-grenoble.fr>
2018-10-27 19:31:05 +0200

    recherche produit

--- tmp/M3104-SiteWeb/view/categories.view.php
+++ tmp/M3104-SiteWeb/view/categories.view.php
@@ -11,8 +11,8 @@
 <header>
 <h2>Chercher un produit :</h2>
 <form class="" action="index.html" method="post">
-  <label for="Choix">Catégories :</label>
-  <select class="Choix" name="Choix">
+  <label for="ChoixCat">Catégories :</label>
+  <select class="ChoixCat" name="ChoixCat">
     <option value="Botte">Botte</option>
     <option value="Mocassin">Mocassin</option>
     <option value="Chausson">Chausson</option>
@@ -20,8 +20,48 @@
     <option value="Tong">tong</option>
     <option value="Chaussure de sport">Chaussure de sport</option>
     <option value="Chaussure de ville">Chaussure de ville</option>
+  </select>
+<br>
+  <label for="ChoixLieu">Lieu disponible :</label>
+  <select class="ChoixLieu" name="ChoixLieu">
+    <option value="Grenoble">Grenoble</option>
+    <option value="Paris">Paris</option>
+    <option value="Lyon">Lyon</option>
+    <option value="Marseille">Marseille</option>
+    <option value="Toulouse">Toulouse</option>
+    <option value="Nice">Nice</option>
+    <option value="Nantes">Nantes</option>
+    <option value="Montpellier">Montpellier</option>
+  </select>
+  <br>
+  <label for="ChoixMarque">Marque :</label>
+  <select class="ChoixMarque" name="ChoixMarque">
+    <option value="Clarks">Clarks</option>
+    <option value="Georgia Rose">Georgia Rose</option>
+    <option value="Esprit">Esprit</option>
+    <option value="Massimo Dutti">Massimo Dutti</option>
+    <option value="Emporio Armani ">Emporio Armani </option>
+    <option value="Enzo Marconi">Enzo Marconi</option>
+    <option value="Giesswein">Giesswein</option>
+    <option value="Magnum">Magnum</option>
+    <option value="Birkenstock">Birkenstock</option>
+    <option value="Superdry">Superdry</option>
+    <option value="Birkenstock">Birkenstock</option>
+    <option value="Asics">Asics</option>
+    <option value="Salomon">Salomon</option>
+    <option value="Nike">Nike</option>
+    <option value="Timberland">Timberland</option>
+    <option value="Pier One">Pier One</option>
 
   </select>
+  <br>
+    <label for="ChoixPrix">Prix :</label>
+    <select class="ChoixPrix" name="ChoixPrix">
+      <option value="50">Moins de 50$</option>
+      <option value="100">Entre 50$ et 150$</option>
+      <option value="150">Plus de 150$</option>
+    </select>
+    <input type="submit"  value="valider recherche">
 </form>
 </header>
 <h1>Catégories :</h1>
