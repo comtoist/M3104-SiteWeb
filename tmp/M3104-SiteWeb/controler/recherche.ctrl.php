e3fe6e4a073ca22a2011bdf6866a76afda36ac88
BULOT Andy <bulota@hostname.iut2.upmf-grenoble.fr>
2018-10-29 12:23:23 +0100

    modified:   controler/recherche.ctrl.php

--- tmp/M3104-SiteWeb/controler/recherche.ctrl.php
+++ tmp/M3104-SiteWeb/controler/recherche.ctrl.php
@@ -1,27 +1,21 @@
 <?php
-    // Partie principale
+// Partie principale
 
-    // Inclusion du modèle
-    include_once("../model/DAO.class.php");
-    $cat = $_GET['cat'];
-    $lieu = $_GET['lieu'];
-    $marque = $_GET['marque'];
-    $prixMIN = (int) $_GET['prixMIN'];
-    $prixMAX = (int) $_GET['prixMAX'];
-    ///////////////////////////////////////////////////////
-    //  A COMPLETER
-    ///////////////////////////////////////////////
+// Inclusion du modèle
+include_once("../model/DAO.class.php");
+$cat = $_GET['cat'];
+$lieu = $_GET['lieu'];
+$marque = $_GET['marque'];
+$prixMIN = (int) $_GET['prixMIN'];
+$prixMAX = (int) $_GET['prixMAX'];
 
-    echo $cat;
-    echo $lieu;
-    echo $marque;
-    echo $prixMIN;
-    echo $prixMAX;
-    global $arti;
-    $arti = $dao->getChercher($cat,$lieu,$marque,$prixMIN,$prixMAX);
+global $arti;
+$arti = $dao->getChercher($cat,$lieu,$marque,$prixMIN,$prixMAX);
+if (($arti)!=null) {
+  include('../view/recherche.view.php');
+}else{
+  include('../view/recherche.view.php');
+  echo "Pas de d'articles trouvés avec vos critères";
+}
 
-
-
-    include('../view/recherche.view.php');
-
-    ?>
+?>
